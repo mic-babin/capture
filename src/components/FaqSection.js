a2b5cfbf15d4f5b791f20f30b20af8143865b14c
mic-babin <mic.babin@outlook.com>
2020-11-18 12:57:21 -0500

    Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">How Do I start?</div>
+      <div className="answer">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet,
+          modi.
+        </p>
+      </div>
+      <div className="question">Daily Schedule.</div>
+      <div className="answer">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet,
+          modi.
+        </p>
+      </div>
+      <div className="question">Different Payment Methods.</div>
+      <div className="answer">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet,
+          modi.
+        </p>
+      </div>
+      <div className="question">What Products do you Offer?</div>
+      <div className="answer">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet,
+          modi.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
