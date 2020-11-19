78d765b1644c87507b7ca2d561a0a00e4f3d51c9
mic-babin <mic.babin@outlook.com>
2020-11-19 11:10:57 -0500

    9. React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
+import { Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 // Import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/contact" exact>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
