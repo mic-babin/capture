5b584c969895d9993ae539d14f99ac5b9e07068e
mic-babin <mic.babin@outlook.com>
2020-11-20 07:14:02 -0500

    16. Wave Animation

--- src/animation.js
+++ src/animation.js
@@ -7,9 +7,9 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 1,
+      duration: 0.75,
       when: "beforeChildren",
-      staggerChildren: 0.25,
+      staggerChildren: 0.35,
     },
   },
   exit: {
@@ -32,7 +32,7 @@ export const fade = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
-    transition: { ease: "easeOut", duration: 0.75 },
+    transition: { ease: "easeOut", duration: 1 },
   },
 };
 
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,36 @@
 import React from "react";
 import home1 from "../img/home1.png";
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 import { Layout, Description, Image, Hide } from "../styles";
-
+import Wave from "./Wave";
 const AboutSection = () => {
   return (
     <Layout>
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span>
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>come true.</motion.h2>
+            <motion.h2 variants={titleAnim}>come true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Photographer" />
+        <motion.img variants={photoAnim} src={home1} alt="Photographer" />
       </Image>
+      <Wave></Wave>
     </Layout>
   );
 };
